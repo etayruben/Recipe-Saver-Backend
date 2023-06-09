6f8859a72e81cc83d90182a5a8d982678f2903d8
Etay Ruben <etayruben@gmail.com>
2023-06-09 20:42:15 +0300

    Created a Mock BE server for the front-end

--- main.ts
+++ main.ts
@@ -11,6 +11,46 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express!');
 });
 
+app.get('/addRecipe', (req: Request, res: Response) => {
+  res.send("Recipe Added")
+})
+
+app.get('/getRecipe', (req: Request, res: Response) => {
+  res.send({"headLine": "Recipe headline!",
+   "link": "https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/", 
+
+   "imageRaw": "https://sallysbakingaddiction.com/wp-content/uploads/2017/04/easy-homemade-soft-pretzels.jpg", 
+   
+   "categories": ["Sweat", "Sour"],
+    "workTime": 25
+  })
+})
+
+app.get('/loadRecipes', (req: Request, res: Response) => {
+  res.send([{"headLine": "Recipe headline!",
+  "link": "https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/", 
+
+  "imageRaw": "https://sallysbakingaddiction.com/wp-content/uploads/2017/04/easy-homemade-soft-pretzels.jpg", 
+  
+  "categories": ["Sweat", "Sour"],
+   "workTime": 25
+ },
+ {"headLine": "Recipe headline!",
+   "link": "https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/", 
+
+   "imageRaw": "https://sallysbakingaddiction.com/wp-content/uploads/2017/04/easy-homemade-soft-pretzels.jpg", 
+   
+   "categories": ["Sweat", "Sour"],
+    "workTime": 25
+  },{"headLine": "Recipe headline!",
+  "link": "https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/", 
+
+  "imageRaw": "https://sallysbakingaddiction.com/wp-content/uploads/2017/04/easy-homemade-soft-pretzels.jpg", 
+  
+  "categories": ["Sweat", "Sour"],
+   "workTime": 25
+ }])
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
