ba0509b12313cdb0fe61e784c9c2635a512ae23c
Etay Ruben <etayruben@gmail.com>
2023-06-12 14:31:52 +0300

    added endpoints for insert and queries

--- main.ts
+++ main.ts
@@ -1,22 +1,30 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import {insertRecipe, queryRecipes} from "./db/api"
 
 const app = express();
 const port = 5500;
 
 app.use(express.json());
-
+  
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express!');
 });
 
-app.get('/addRecipe', (req: Request, res: Response) => {
-  res.send('Recipe Added');
+app.post("/addMultipleRecipes", (req: Request, res: Response) => {
+  res.send("Not implemented yet")
+})
+
+app.post('/addRecipe', (req: Request, res: Response) => {
+  console.info(req);
+  insertRecipe(req.body)
+  res.send("Recipe Added!")
 });
 
 app.get('/getRecipe', (req: Request, res: Response) => {
+
   res.send({
     headline: 'Recipe headline!',
     link: 'https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/',
@@ -28,6 +36,8 @@ app.get('/getRecipe', (req: Request, res: Response) => {
 });
 
 app.get('/loadRecipes', (req: Request, res: Response) => {
+  res.send(queryRecipes({}, {}))
+  return 
   res.send([
     {
       headline: 'Recipe headline!',
