367621db87ad522df5c6510e2c4828ceb7052362
Etay Ruben <etayruben@gmail.com>
2023-06-12 18:06:00 +0300

    removed getRecipe, usused

--- main.ts
+++ main.ts
@@ -9,30 +9,18 @@ app.use(express.json());
   
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express!');
+app.get('/health_check', (req: Request, res: Response) => {
+  res.send('Server is up and running');
 });
 
 app.post("/addMultipleRecipes", (req: Request, res: Response) => {
   res.send("Not implemented yet")
 })
 
-app.post('/addRecipe', (req: Request, res: Response) => {
+app.post('/addRecipe', async (req: Request, res: Response) => {
   console.info(req);
-  insertRecipe(req.body)
+  await insertRecipe(req.body)
   res.send("Recipe Added!")
-});
-
-app.get('/getRecipe', (req: Request, res: Response) => {
-
-  res.send({
-    headline: 'Recipe headline!',
-    link: 'https://sallysbakingaddiction.com/easy-homemade-soft-pretzels/',
-    imageRaw:
-      'https://sallysbakingaddiction.com/wp-content/uploads/2017/04/easy-homemade-soft-pretzels.jpg',
-    categories: ['Sweat', 'Sour'],
-    workTime: 25,
-  });
 });
 
 app.post('/loadRecipes', async (req: Request, res: Response) => {
